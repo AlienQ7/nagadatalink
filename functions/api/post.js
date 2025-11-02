b1a2a61b042cfec659af190b4653caf328e88e06
AlienQ7 <celestialq7@gmail.com>
2025-11-02 15:35:04 +0530

    Create post.js

---
+++ functions/api/post.js
@@ -0,0 +1,23 @@
+2025-11-02T09:55:40.18522Z	Cloning repository...
+2025-11-02T09:55:40.782797Z	From https://github.com/AlienQ7/nagadatalink
+2025-11-02T09:55:40.783262Z	 * branch            85dae48189557b56ffcbfae617ffb1ad57262155 -> FETCH_HEAD
+2025-11-02T09:55:40.783356Z	
+2025-11-02T09:55:40.809773Z	HEAD is now at 85dae48 Update sell.html
+2025-11-02T09:55:40.810192Z	
+2025-11-02T09:55:40.885502Z	
+2025-11-02T09:55:40.886087Z	Using v2 root directory strategy
+2025-11-02T09:55:40.907489Z	Success: Finished cloning repository files
+2025-11-02T09:55:42.862771Z	Checking for configuration in a Wrangler configuration file (BETA)
+2025-11-02T09:55:42.863585Z	
+2025-11-02T09:55:42.864852Z	Found wrangler.toml file. Reading build configuration...
+2025-11-02T09:55:42.871781Z	pages_build_output_dir: public
+2025-11-02T09:55:42.871956Z	Build environment variables: (none found)
+2025-11-02T09:55:43.974521Z	Successfully read wrangler.toml file.
+2025-11-02T09:55:43.975123Z	No build command specified. Skipping build step.
+2025-11-02T09:55:43.975759Z	Found Functions directory at /functions. Uploading.
+2025-11-02T09:55:43.981597Z	 ⛅️ wrangler 3.101.0
+2025-11-02T09:55:43.981824Z	-------------------
+2025-11-02T09:55:44.937015Z	✨ Compiled Worker successfully
+2025-11-02T09:55:46.002335Z	Validating asset output directory
+2025-11-02T09:55:46.002639Z	Error: Output directory "public" not found.
+2025-11-02T09:55:49.024648Z	Failed: build output directory not found
